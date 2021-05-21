87e8709a1d43694d13239778cf73a5eeec21b11c
mark <mark.stanislawek@gmail.com>
2021-05-21 18:13:40 -0500

    complete project work

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,23 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x = 10, start_y = 10, end_x = 90, end_y = 90;
+    std::cout << "For each of the following prompts, please enter number between 0.0 and 100.0 inclusive" << std::endl;
+    std::cout << "Please enter starting value for x between 0 and 100: ";
+    std::cin >> start_x;
+    std::cout << "Now please enter a starting value for y: ";
+    std::cin >> start_y;
+    std::cout << "Please enter an ending value for x: ";
+    std::cin >> end_x;
+    std::cout << "Finally, please enter an ending value y: ";
+    std::cin >> end_y;
+    std::cout << "Using starting x,y values: " << start_x << ", " << start_y << " and ending x,y values: " << end_x << ", " << end_y << std::endl;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
