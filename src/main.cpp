e17fbe0dd09330551beb2c9270541abb6614f51e
mark <mark.stanislawek@gmail.com>
2021-05-23 18:21:20 -0500

    some clean up for final version.

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,107 @@
-#include <optional>
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <io2d.h>
-#include "route_model.h"
 #include "render.h"
+#include "route_model.h"
 #include "route_planner.h"
+#include <fstream>
+#include <io2d.h>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
 
 using namespace std::experimental;
 
-static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
-    std::ifstream is{path, std::ios::binary | std::ios::ate};
-    if( !is )
-        return std::nullopt;
-    
-    auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
-    is.seekg(0);
-    is.read((char*)contents.data(), size);
+static std::optional<std::vector<std::byte>> ReadFile(const std::string &path) {
+  std::ifstream is{path, std::ios::binary | std::ios::ate};
+  if (!is)
+    return std::nullopt;
 
-    if( contents.empty() )
-        return std::nullopt;
-    return std::move(contents);
+  auto size = is.tellg();
+  std::vector<std::byte> contents(size);
+
+  is.seekg(0);
+  is.read((char *)contents.data(), size);
+
+  if (contents.empty())
+    return std::nullopt;
+  return std::move(contents);
 }
 
-int main(int argc, const char **argv)
-{    
-    std::string osm_data_file = "";
-    if( argc > 1 ) {
-        for( int i = 1; i < argc; ++i )
-            if( std::string_view{argv[i]} == "-f" && ++i < argc )
-                osm_data_file = argv[i];
-    }
-    else {
-        std::cout << "To specify a map file use the following format: " << std::endl;
-        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
-        osm_data_file = "../map.osm";
-    }
-    
-    std::vector<std::byte> osm_data;
- 
-    if( osm_data.empty() && !osm_data_file.empty() ) {
-        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
-        auto data = ReadFile(osm_data_file);
-        if( !data )
-            std::cout << "Failed to read." << std::endl;
-        else
-            osm_data = std::move(*data);
-    }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
-    float start_x = 10, start_y = 10, end_x = 90, end_y = 90;
-    std::cout << "For each of the following prompts, please enter number between 0.0 and 100.0 inclusive" << std::endl;
-    std::cout << "Please enter starting value for x between 0 and 100: ";
-    std::cin >> start_x;
-    std::cout << "Now please enter a starting value for y: ";
-    std::cin >> start_y;
-    std::cout << "Please enter an ending value for x: ";
-    std::cin >> end_x;
-    std::cout << "Finally, please enter an ending value y: ";
-    std::cin >> end_y;
-    std::cout << "Using starting x,y values: " << start_x << ", " << start_y << " and ending x,y values: " << end_x << ", " << end_y << std::endl;
+bool isRangeValid(float input_x, float input_y) {
+  return input_x >= 0.0 && input_y >= 0.0 && input_x <= 100.0 &&
+         input_y <= 100.0;
+}
 
-    // Build Model.
-    RouteModel model{osm_data};
+void getInput(const std::string prompt, float &x, float &y) {
+  float input_x = 0.0, input_y = 0.0;
+  do {
+    std::cout << "Please enter a number for the " << prompt
+              << " x coordinate: ";
+    std::cin >> input_x;
+    std::cout << "Please enter a number for the " << prompt
+              << " y coordinate: ";
+    std::cin >> input_y;
+  } while (!isRangeValid(input_x, input_y));
 
-    // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
-    route_planner.AStarSearch();
+  std::cout << "Using " << prompt << " x,y coordinates: " << input_x << ","
+            << input_y << std::endl;
+  x = input_x;
+  y = input_y;
+}
 
-    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+int main(int argc, const char **argv) {
+  std::string osm_data_file = "";
+  if (argc > 1) {
+    for (int i = 1; i < argc; ++i)
+      if (std::string_view{argv[i]} == "-f" && ++i < argc)
+        osm_data_file = argv[i];
+  } else {
+    std::cout << "To specify a map file use the following format: "
+              << std::endl;
+    std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+    osm_data_file = "../map.osm";
+  }
 
-    // Render results of search.
-    Render render{model};
+  std::vector<std::byte> osm_data;
 
-    auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
-    display.size_change_callback([](io2d::output_surface& surface){
-        surface.dimensions(surface.display_dimensions());
-    });
-    display.draw_callback([&](io2d::output_surface& surface){
-        render.Display(surface);
-    });
-    display.begin_show();
+  if (osm_data.empty() && !osm_data_file.empty()) {
+    std::cout << "Reading OpenStreetMap data from the following file: "
+              << osm_data_file << std::endl;
+    auto data = ReadFile(osm_data_file);
+    if (!data)
+      std::cout << "Failed to read." << std::endl;
+    else
+      osm_data = std::move(*data);
+  }
+
+  float start_x = 10, start_y = 10, end_x = 90, end_y = 90;
+  std::cout << "Please enter numbers between 0.0 and 100.0 inclusive for the "
+               "following..."
+            << std::endl;
+  getInput("starting", start_x, start_y);
+  getInput("ending", end_x, end_y);
+
+  // Build Model.
+  RouteModel model{osm_data};
+
+  // Create RoutePlanner object and perform A* search.
+  RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
+  route_planner.AStarSearch();
+
+  std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+
+  // Render results of search.
+  Render render{model};
+
+  auto display = io2d::output_surface{400,
+                                      400,
+                                      io2d::format::argb32,
+                                      io2d::scaling::none,
+                                      io2d::refresh_style::fixed,
+                                      30};
+  display.size_change_callback([](io2d::output_surface &surface) {
+    surface.dimensions(surface.display_dimensions());
+  });
+  display.draw_callback(
+      [&](io2d::output_surface &surface) { render.Display(surface); });
+  display.begin_show();
 }
